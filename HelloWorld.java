cbc8fd298e5833855ecc5e844706742f09c12761
zheng yuan <zheng_yuan_yuan@outlook.com>
2017-12-07 21:26:20 +0800

    1st

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+package com.itheima.test;
+
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       /* System.out.println("hello world");
+        List<String> ar = new ArrayList<>();
+        for (String s : ar) {
+
+        }*/
+        String s = new String("1234");
+
+        //1.万能键  ALT +ENTER  (包括导入包 ：实现接口的实现类 )
+        //2.CTR+N 找类
+        //3.CTR+SHIFT+N 找文件
+        //4.CTR+ALT+V : 自动补全
+        //5.循环遍历 迭代 ITER
+
+        //6.CTR+J 提示快捷键
+
+        //7.Ctr+/  注释和放开注释  行注释
+
+        //8.ALT + INSERT 生成POJO的getter和setter
+
+        //9.CTR+ALT+B ：查看接口或者抽象类的子类
+    }
+}
